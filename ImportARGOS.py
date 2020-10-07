11d38c20d3dff8fa8b8b9336ea15aedff3bfa76a
srriddick <sage.riddick@duke.edu>
2020-10-06 21:29:28 -0400

    Import modules

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -9,3 +9,6 @@
 ## Created: Fall 2020
 ## Author: sage.riddick@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
+
+# Import modules
+import sys, os, arcpy
